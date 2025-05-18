a335c5d599405ff791317ce69b22baddcb2307b3
Sandeep <gautamsandeep2021@gmail.com>
2025-05-18 17:07:40 +0530

    fix loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 
-const Loader = ({size}) => {
+const Loader = ({size = 16}) => {
   return (
     <>
       <div className={`w-${size} h-${size} border-4 border-gray-200 border-t-blue-500 rounded-full animate-spin mx-auto`}></div>
